5522702cf1087409ba01d6a0a497b043a67302ef
QJoly <quentinjoly@thoughtless.eu>
2022-09-11 15:19:59 +0200

    Usage of an instance to interact with the object trilium

--- start.py
+++ start.py
@@ -6,11 +6,12 @@ from trilium import triliuminstance
 logging.basicConfig(filename='debug.log', encoding='utf-8', level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
-
 def main():
     url, token = config().get_auth()
-    instance = triliuminstance(url, token)
-
+    instance = triliuminstance(url, token).get_instance()
+    instance.read_note("VL4p6pb83Yya")
+    instance.update_note(config())
+#    instance.search_note("Python")
 
 if __name__ == "__main__":
     main()
